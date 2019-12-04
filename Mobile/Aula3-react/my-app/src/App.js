f213738fb41d3f3911a993c5f098b64a0b2526f5
giovane <giovane.sobrinho@gmail.com>
2019-12-04 12:25:28 -0300

    up

--- Mobile/Aula3-react/my-app/src/App.js
+++ Mobile/Aula3-react/my-app/src/App.js
@@ -1,6 +1,75 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
-export default App;
+
+//não tem o default tem que usar desconstruido na chamada e fazer a chamada
+
+//Componentes de Classe
+export class CadastroUsuario extends Component {
+  constructor(){
+    super()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.cadastrarUsuario}>
+        <input type="text" placeholder="Nome" />
+        <input type="submit" />
+      </form>
+    );
+  }
+  cadastrarUsuario(evento){
+    evento.preventDefault();
+    console.log(evento.target.value)
+  }
+}
+
+export class ListagemUsuario extends Component {
+  constructor(){
+    super()
+    this.state = { pessoas: ["João","Pedro","Giovane","Maiana","Rosa","Bananinha"]}
+  }
+  
+  
+  render() {
+    return (
+      <ul>
+        {this.state.pessoas.map(nome => <li>{nome}</li>)}
+        
+      </ul>
+    );
+  }
+}
+
+
+//Componentes de classe e componentes funcionais são os dois tipos de componentes que existem
+//componentes de classe
+
+// toda classe tem que extender de component
+/* class App extends Component{  
+  //metodo responsavel para renderizar as coisas  na tela  
+  render(){
+      return (
+      <div>
+      <p> Olá, Olá</p>        
+      </div>
+      );
+    }
+} */
+
+// Componentes funcionais
+
+/* export default function App(){
+  
+    return (
+    <div>
+    <p> Olá, Olá, Olê</p>        
+    </div>
+    );
+  } */
+
+//se emportar na funcção não precisa no final, manter o nome da classe e da função no export
+/* export default App;
+ */
--- Mobile/Aula3-react/my-app/src/index.js
+++ Mobile/Aula3-react/my-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {CadastroUsuario, ListagemUsuario} from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div><CadastroUsuario /> <ListagemUsuario /></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

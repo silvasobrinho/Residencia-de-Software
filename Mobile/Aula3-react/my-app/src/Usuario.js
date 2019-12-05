11fc829cc9bb7de8a7884b4bb31e1a808422f020
giovane <giovane.sobrinho@gmail.com>
2019-12-05 11:11:59 -0300

    aula03-mobile set-State

--- Mobile/Aula3-react/my-app/src/Usuario.js
+++ Mobile/Aula3-react/my-app/src/Usuario.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 //Componentes de Classe
@@ -7,40 +6,64 @@ export default class CadastroUsuario extends Component {
   constructor() {
     super();
     this.state = { nome: "", pessoas: ["Maria", "Pedro", "Joana"] };
-      
+
     this.cadastrarUsuario = this.cadastrarUsuario.bind(this);
+    this.remover = this.remover.bind(this);
+    this.guardovalor = this.guardovalor.bind(this);
   }
 
 
   cadastrarUsuario(evento) {
     evento.preventDefault();
-    //tem que setar o state cabaço, assim não vai
-   /*  this.state.pessoas.push(evento.target[0].value); */
 
-   this.setState({
-     pessoas: this.state.pessoas.concat([evento.target[0].value])
-   })
-  
-    
+    //para não escrever quando o cmapo esta vazio 
+    if (evento.target[0].value) {
+      console.log(evento.target[0].value)
+      this.setState({
+        pessoas: this.state.pessoas.concat([evento.target[0].value])
+      })
+    }
+  }
+
+  remover(evento) {
+    evento.preventDefault();
+    let pessoas = this.state.pessoas;
+    let pessoaEditar = this.state.inputValue;
+    let acheiIndex = pessoas.findIndex(x => x == pessoaEditar);
+    //removo do vetor
+    this.state.pessoas.splice(acheiIndex, 1);
+    //se não existir não retira
+    if (acheiIndex !== -1) {
+      this.setState({
+        pessoas: this.state.pessoas
+
+      })
+    }
+
+  }
+
+  guardovalor(evento) {
+    this.setState({
+      inputValue: evento.target.value
+    });
   }
 
   render() {
     return (
       <>
         <form onSubmit={this.cadastrarUsuario}>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state.value} onChange={this.guardovalor} />
           <input type="submit" />
+          <button onClick={this.remover}>Remover</button>
         </form>
         <ul>
           {this.state.pessoas.map(nome => (
             <li>{nome}</li>
-            
+
           ))}
-           
+
         </ul>
       </>
     );
   }
-  
-  
 }
